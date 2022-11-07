79b647e4c97c47b27f39a61f050f25bf03a34b31
Kubra Hanedar <the.haneda@gmail.com>
2022-11-07 18:44:10 +0300

    ilk sayfa oluşturuldu.

---
+++ GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GestureRecognizerApp
+//
+//  Created by Kübra Hanedar on 6.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    
+}
+
